d2d2823af32976f9615fe6def4e044209baae0e1
Alexander Drakon <drakon.a.v@gmail.com>
2023-10-30 17:16:32 +0300

    feat: errors

--- utils/errors.js
+++ utils/errors.js
@@ -3,23 +3,31 @@ const { STATUSES } = require('./statuses');
 module.exports.handleUserErrors = (err, res) => {
   if (err.name === 'ValidationError') {
     res.status(STATUSES.BAD_REQUEST).send({ message: `Недопустимые данные пользователя: ${err.message}` });
-    return;
+    return
   }
   if (err.name === 'CastError') {
-    res.status(STATUSES.NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
-    return;
+    res.status(STATUSES.BAD_REQUEST).send({ message: 'Неверный формат ID пользователя' });
+    return
   }
-  res.status(STATUSES.INTERNAL).send({ message: 'Неизвестная ошибка' });
-};
+  if (err.name === 'DocumentNotFoundError') {
+    res.status(STATUSES.NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
+    return
+  }
+  res.status(STATUSES.INTERNAL).send({ message: 'Неизвестная ошибка'});
+}
 
 module.exports.handleCardErrors = (err, res) => {
   if (err.name === 'ValidationError') {
-    res.status(STATUSES.BAD_REQUEST).send({ message: `Недопустимые данные пользователя: ${err.message}` });
-    return;
+    res.status(STATUSES.BAD_REQUEST).send({ message: `Недопустимые данные карточки: ${err.message}` });
+    return
   }
   if (err.name === 'CastError') {
-    res.status(STATUSES.NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
-    return;
+    res.status(STATUSES.BAD_REQUEST).send({ message: 'Неверный формат ID карточки' });
+    return
   }
-  res.status(STATUSES.INTERNAL).send({ message: 'Неизвестная ошибка' });
-};
+  if (err.name === 'DocumentNotFoundError') {
+    res.status(STATUSES.NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
+    return
+  }
+  res.status(STATUSES.INTERNAL).send({ message: 'Неизвестная ошибка'});
+}
